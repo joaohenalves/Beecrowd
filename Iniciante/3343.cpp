7279d7f4e50c35e4683c7efbd6896513f5c9301c
João Henrique Alves dos Santos <jaozao77@gmail.com>
2023-11-30 11:09:41 -0300

    Update 3343.cpp

--- Iniciante/3343.cpp
+++ Iniciante/3343.cpp
@@ -5,23 +5,23 @@ using namespace std;
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, w, s, c;
+    int n, w, s, c, p, m, g;
     string titans;
-    unordered_map<char, int> sz;
-    unordered_map<char, int> id;
     cin >> n >> w >> titans;
     int walls[n + 1];
     int pos[3] = {1, 1, 1};
     for(int i = 0; i < n + 1; i++) walls[i] = w;
-    cin >> s; sz['P'] = s; id['P'] = 0;
-    cin >> s; sz['M'] = s; id['M'] = 1;
-    cin >> s; sz['G'] = s; id['G'] = 2;
+    cin >> s; p = s;
+    cin >> s; m = s;
+    cin >> s; g = s;
     for(int i = 0; i < n; i++) {
-        c = id[titans[i]];
-        while(walls[pos[id[titans[i]]]] < sz[titans[i]]) {
-            pos[id[titans[i]]]++;
+        if(titans[i] == 'P') { s = p; c = 0; }
+        else if(titans[i] == 'M') { s = m; c = 1; }
+        else if(titans[i] == 'G') { s = g; c = 2; }
+        while(walls[pos[c]] < s) {
+            pos[c]++;
         }
-        walls[pos[id[titans[i]]]] -= sz[titans[i]];
+        walls[pos[c]] -= s;
     }
     cout << max(pos[0], max(pos[1], pos[2])) << "\n";
     return 0;
