7176e55289bb6cc0ef02746b1da070727d8d0d0c
João Henrique Alves dos Santos <jaozao77@gmail.com>
2023-12-03 18:29:05 -0300

    Create 1100_bfs.cpp

---
+++ Grafos/1100_bfs.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 64
+
+int dist[MAX][MAX];
+int marked[MAX][MAX];
+int mvx[] = {-2, -2, -1, -1, 1, 1, 2, 2};
+int mvy[] = {-1, 1, -2, 2, -2, 2, -1, 1};
+
+void bfs(int i, int j) {
+    int ii, jj, src = i * 8 + j, aux = 7 * 8 + 7;
+    pair<int, int> temp;
+    queue<pair<int, int> > q;
+    dist[src][src] = 0;
+    dist[aux - src][aux - src] = 0;
+    marked[src][src] = 1;
+    q.emplace(i, j);
+    while(!q.empty()) {
+        temp = q.front();
+        q.pop();
+        for(int k = 0; k < 8; k++) {
+            ii = temp.first + mvy[k];
+            jj = temp.second + mvx[k];
+            if(ii >= 0 && ii < 8 && jj >= 0 && jj < 8 && marked[src][ii * 8 + jj] == 0) {
+                dist[src][ii * 8 + jj] = dist[src][temp.first * 8 + temp.second] + 1;
+                dist[aux - src][aux - (ii * 8 + jj)] = dist[aux - src][aux - (temp.first * 8 + temp.second)] + 1;
+                marked[src][ii * 8 + jj] = 1;
+                q.emplace(ii, jj);
+            }
+        }
+    }
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    string src, dst;
+    memset(marked, 0, sizeof(marked));
+    for(int i = 0; i < 8; i++) {
+        for(int j = 0; j <= i; j++) {
+            bfs(i, j);
+        }
+    }
+    while(cin >> src >> dst) {
+        int a = ((src[1] - '1') * 8) + src[0] - 'a';
+        int b = ((dst[1] - '1') * 8) + dst[0] - 'a';
+        cout << "To get from " << src << " to " << dst << " takes " << dist[a][b] << " knight moves.\n";
+    }
+    return 0;
+}
