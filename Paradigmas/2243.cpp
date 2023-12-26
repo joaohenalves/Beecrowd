8f8cb75aff71e16e5658dc87de6f10a2ebe8c2f3
João Henrique Alves dos Santos <jaozao77@gmail.com>
2023-12-26 13:02:50 -0300

    Create 2243.cpp

---
+++ Paradigmas/2243.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int heights[51234];
+    int n, temp, mx = 0;
+    cin >> n;
+    heights[0] = heights[n + 1] = 0;
+    for(int i = 1; i <= n; i++) {
+        cin >> temp;
+        heights[i] = min(heights[i - 1] + 1, temp);
+    }
+    for(int i = n; i > 0; i--) {
+        heights[i] = min(heights[i + 1] + 1, heights[i]);
+        if(heights[i] > mx) mx = heights[i]; 
+    }
+    cout << mx << "\n";
+    return 0;
+}
