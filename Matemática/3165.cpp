59d14a5d60c82cbaf53ef2b6b075cfbf4034c28a
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-01-16 09:04:06 -0300

    Update 3165.cpp

--- Matemática/3165.cpp
+++ Matemática/3165.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 
 bool isPrime[1123];
+int n;
 
 void sieve() {
     memset(isPrime, 1, sizeof(isPrime));
-    for(int i = 2; i * i <= 1123; i++) {
+    for(int i = 2; i * i <= n; i++) {
         if(isPrime[i]) {
-            for(int j = i * i; j < 1123; j += i) {
+            for(int j = i * i; j <= n; j += i) {
                 isPrime[j] = 0;
             }
         }
@@ -18,22 +19,13 @@ void sieve() {
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    int n, f = 0, s = 0;
-    sieve();
     cin >> n;
+    sieve();
     for(int i = n; i >= 2; i--) {
-        if(isPrime[i]) {
-            if(f == 0) f = i;
-            else if(s == 0) {
-                s = i;
-                if(f - s == 2) break;
-            } else {
-                f = s;
-                s = i;
-                if(f - s == 2) break;
-            }
+        if(isPrime[i] && isPrime[i - 2]) {
+            cout << i - 2 << " " << i << "\n";
+            break;
         }
     }
-    cout << s << " " << f << "\n";
     return 0;
 }
