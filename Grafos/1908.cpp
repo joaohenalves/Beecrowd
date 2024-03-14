53e2cd99dfd052729171cb97829958cb34a0853f
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-03-14 17:13:43 -0300

    Update 1908.cpp

--- Grafos/1908.cpp
+++ Grafos/1908.cpp
@@ -1,58 +1,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, counter = 0;
-bitset<10000> visited;
+#define MAX 11234
+#define MAX_K 1123
+typedef pair<int, int> ii;
 
-void bfs(vector<vector<int> > &graph, vector<int> &dist) {
-    queue<int> qu;
-    visited[0] = 1;
-    dist[0] = 0;
-    qu.emplace(0);
-    while(!qu.empty()) {
-        int temp = qu.front();
-        qu.pop();
-        for(auto v : graph[temp]) {
-            if(visited[v] == 0) {
-                qu.emplace(v);
-                visited[v] = 1;
-                dist[v] = dist[temp] + 1;
-                if(v == n - 1) return;
+vector<vector<int>> graph(MAX);
+vector<vector<int>> camp(MAX_K);
+int n, k, vis[MAX], line[MAX_K];
+
+int bfs() {
+    queue<ii> q;
+    for(int &l : graph[0]) {
+        line[l] = 1;
+        q.emplace(1, l);
+    }
+    vis[0] = 1;
+    while(!q.empty()) {
+        auto [ld, lq] = q.front();
+        q.pop();
+        for(int &v : camp[lq]) {
+            if(v == n - 1) return ld;
+            if(vis[v] == 0) {
+                for(int &l : graph[v]) {
+                    if(line[l] == 0) {
+                        q.emplace(ld + 1, l);
+                        line[l] = 1;
+                    }
+                }
+                vis[v] = 1;
             }
         }
     }
+    return 0;
 }
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
-    int k, n1, n2;
+    int l, u;
+    memset(vis, 0, sizeof(vis));
+    memset(line, 0, sizeof(line));
     cin >> n >> k;
-    vector<vector<int> > graph;
-    vector<int> emptyList;
-    vector<int> dist (n);
-    graph.reserve(n);
-    for(int i = 0; i <= n; i++) {
-        graph.emplace_back(emptyList);
-    }
-    vector<int> tempList;
-    for(int j = 0; j < k; j++) {
-        cin >> n1;
-        for(int l = 0; l < n1; l++) {
-            cin >> n2;
-            tempList.emplace_back(n2 - 1);
+    for(int i = 0; i < k; i++) {
+        cin >> l;
+        for(int j = 0; j < l; j++) {
+            cin >> u;
+            u--;
+            graph[u].push_back(i);
+            camp[i].push_back(u);
         }
-        for(int v = 0; v < n1; v++) {
-            for(int b = 0; b < n1; b++) {
-                if(tempList[b] != tempList[v]) {
-                    graph[tempList[v]].emplace_back(tempList[b]);
-                }
-            }
-        }
-        tempList.clear();
     }
-    bfs(graph, dist);
-    cout << dist[n - 1] << "\n";
+    cout << bfs() << "\n";
     return 0;
 }
