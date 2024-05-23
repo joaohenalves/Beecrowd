132098586a9d9ac8674fc3daa982aaa5e8857241
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-23 15:58:12 -0300

    calculo de gcd - teorema sobre fibonacci

---
+++ Ad-hoc/3471.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int gcd(int a, int b) {
+    if(b == 0) return a;
+    return gcd(b, a % b);
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b;
+    cin >> a >> b;
+    cout << gcd(a, b) << "\n";
+    return 0;
+}
