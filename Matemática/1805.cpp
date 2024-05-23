eaefa5a46d3fceb4a18af316a0aad79db7a8047d
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-23 17:19:06 -0300

    S

---
+++ Matemática/1805.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    ll a, b;
+    cin >> a >> b;
+    cout << (b * (b + 1) / 2) - (a * (a + 1) / 2) + a << "\n";
+    return 0;
+}
