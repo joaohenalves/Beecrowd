0602f1295876189e3983086e89c9cd73f9711525
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-23 11:41:00 -0300

    Update 1104.cpp

--- Ad-hoc/1104.cpp
+++ Ad-hoc/1104.cpp
@@ -1,3 +1,5 @@
+// USANDO SET
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -9,8 +11,8 @@ int main() {
     for(;;) {
         cin >> n >> m;
         if(n == 0) break;
-        unordered_set<int> cards;
-        unordered_set<int> setAux;
+        set<int> cards;
+        set<int> setAux;
         counter = 0;
         for(int i = 0; i < n; i++) {
             cin >> temp;
@@ -25,6 +27,39 @@ int main() {
         sizeAfter = cards.size();
         sizeAux = setAux.size();
         cout << min(sizeAfter - sizeAux, sizeAfter - sizeBefore) << "\n";
+    }
+    return 0;
+}
+
+
+//USANDO BITSET
+
+#include <bits/stdc++.h>
+using namespace std;
+
+bitset<112345> cardsA;
+bitset<112345> cardsB;
+bitset<112345> auxSet;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b, aux, aux2;
+    while(cin >> a >> b, a) {
+        for(int i = 0; i < a; i++) {
+            cin >> aux;
+            cardsA[aux] = 1;
+        }
+        for(int i = 0; i < b; i++) {
+            cin >> aux;
+            cardsB[aux] = 1;
+        }
+        auxSet = cardsB;
+        cardsB &= ~cardsA;
+        cardsA &= ~auxSet;
+        cout << min(cardsA.count(), cardsB.count()) << "\n";
+        cardsA.reset();
+        cardsB.reset();
     }
     return 0;
 }
