e566b208750ae13d68d6eeba2983986330723db9
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-23 11:47:23 -0300

    Create 1379.cpp

---
+++ Ad-hoc/1379.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int a, b;
+    while(cin >> a >> b, a) cout << a - (b - a) << "\n";
+    return 0;
+}
