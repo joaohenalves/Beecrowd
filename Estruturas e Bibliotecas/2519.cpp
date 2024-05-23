07b8fed8ae6573c0a143305609a2d1c4ee0a7496
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-23 16:51:53 -0300

    BIT 2D

---
+++ Estruturas e Bibliotecas/2519.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define MAX 1123
+typedef long long ll;
+
+int n, m, bit[MAX][MAX];
+
+int getbit(int x, int y) {
+    int ans = 0;
+    for(int i = x; i > 0; i -= i & -i) {
+        for(int j = y; j > 0; j -= j & -j) {
+            ans += bit[i][j];
+        }
+    }
+    return ans;
+}
+
+void setbit(int x, int y, int delta) {
+    for(int i = x; i <= n; i += i & -i) {
+        for(int j = y; j <= m; j += j & -j) {
+            bit[i][j] += delta;
+        }
+    }
+}
+ 
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int x, q, x1, y1, x2, y2;
+    while(cin >> n >> m) {
+        for(int i = 0; i <= n; i++) {
+            for(int j = 0; j <= m; j++) {
+                bit[i][j] = 0;
+            }
+        }
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                cin >> x;
+                if(x) setbit(i, j, 1);
+            }
+        }
+        cin >> q;
+        while(q--) {
+            cin >> x >> x1 >> y1 >> x2 >> y2;
+            if(x) {
+                cout << getbit(x2, y2) - getbit(x2, y1 - 1) - getbit(x1 - 1, y2) + getbit(x1 - 1, y1 - 1) << "\n";
+            } else {
+                setbit(x1, y1, -1);
+                setbit(x2, y2, 1);
+            }
+        }
+    }
+    return 0;
+}
