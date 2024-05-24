b0bfbd4fe3ba3a5dea31155ea8e9598998a98642
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-24 14:28:24 -0300

    Radares (iterativo)

--- Paradigmas/1689-radares-iterativo.cpp
+++ Paradigmas/1689-radares-iterativo.cpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int rad[1000001], pos[1000001];
+int rad[1123456], pos[1123456];
 
 
 int main(){
@@ -11,21 +11,20 @@ int main(){
     cin >> t;
     while(t--) {
         cin >> n >> k;
+        memset(rad, 0, sizeof(rad));
         maxPos = 0;
         for(int i = 0; i < n; i++) {
             cin >> pos[i];
             maxPos = max(maxPos, pos[i]);
         }
-        memset(rad, 0, sizeof(rad));
         for(int j = 0; j < n; j++) {
             cin >> l;
             rad[pos[j]] = max(rad[pos[j]], l);
         }
-        for(int i = maxPos; i >= 0; i--) {
-            limit = min(1000001, i + k);
-            rad[i] = max(rad[i + 1], rad[i] + rad[limit]);
+        for(int i = 1; i <= maxPos; i++) {
+            rad[i] = max(rad[i - 1], rad[i] + (i - k >= 0 ? rad[i - k] : 0));
         }
-        cout << rad[0] << "\n";
+        cout << rad[maxPos] << "\n";
     }
     return 0;
 }
