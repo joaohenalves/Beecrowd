e5e93da4cd4e56520ca544c61d3abff4b40a84c3
João Henrique Alves dos Santos <jaozao77@gmail.com>
2024-05-24 16:33:17 -0300

    Grid

--- Grafos/2419.cpp
+++ Grafos/2419.cpp
@@ -1,43 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n, m, counter = 0;
-int si[] = {-1, 1, 0, 0};
-int sj[] = {0, 0, -1, 1};
+#define MAX 1123
 
-void check(int i, int j, vector<string> &grid) {
-    if(min(i, j) == 0 || j == m - 1 || i == n - 1) {
-        counter++;
-        return;
-    } else {
-        int ii, jj;
-        for(int k = 0; k < 4; k++) {
-            ii = i + si[k];
-            jj = j + sj[k];
-            if(grid[ii][jj] == '.') {
-                counter++;
-                return;
+int n, m;
+int ai[] = {-1, 1, 0, 0};
+int aj[] = {0, 0, 1, -1};
+
+vector<string> grid(MAX);
+
+int solve() {
+    int ii, jj, ans = 0;
+    for(int i = 0; i < n; i++) {
+        for(int j = 0; j < n; j++) {
+            if(grid[i][j] == '#') {
+                for(int k = 0; k < 4; k++) {
+                    ii = i + ai[k];
+                    jj = j + aj[k];
+                    if(ii < 0 || ii >= n || jj < 0 || jj >= m || grid[ii][jj] == '.') {
+                        ans++;
+                        break;
+                    }
+                }
             }
         }
     }
+    return ans;
 }
-
 
 int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
-    cout.tie(0);
     cin >> n >> m;
-    vector<string> grid(n);
     for(int i = 0; i < n; i++) {
         cin >> grid[i];
     }
-    for(int i = 0; i < n; i++) {
-        for(int j = 0; j < m; j++) {
-            if(grid[i][j] == '#')
-                check(i, j, grid);
-        }
-    }
-    cout << counter << "\n";
+    cout << solve() << "\n";
     return 0;
 }
